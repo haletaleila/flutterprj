01d5093e33117d29ecdcb1e7d7a24593357ac2b0
haletaleila <leilahaleta@gmail.com>
2023-12-04 15:56:23 +0900

    recap dart

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 
+class Player {
+  late String name;
+  Player({required this.name});
+}
+
 void main() {
+  var ressurd = Player(name: 'ress');
+  ressurd.name;
+
   runApp(App());
 }
 
