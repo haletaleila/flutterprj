e1431df980cc8e333d447e7876b1e4793599224c
haletaleila <leilahaleta@gmail.com>
2023-12-05 10:58:41 +0900

    231205

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -40,6 +41,49 @@ class App extends StatelessWidget {
                       ),
                     ],
                   )
+                ],
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  fontSize: 22,
+                  color: Colors.white.withOpacity(0.8),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 572',
+                style: TextStyle(
+                  fontSize: 42,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(45),
+                        color: Colors.amber,
+                      ),
+                      child: Padding(
+                        padding: EdgeInsets.symmetric(
+                          vertical: 20,
+                          horizontal: 50,
+                        ),
+                        child: Text('Transfer',
+                            style: TextStyle(
+                              fontSize: 20,
+                            )),
+                      )),
                 ],
               )
             ],
